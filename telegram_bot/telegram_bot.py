473bab36924dccdbde41995a766504ff54283300
bazilyq <bazilyqq@gmail.com>
2024-01-13 14:21:12 +0600

    added model and blur to bot

--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -1,20 +1,22 @@
 import os
 import telebot
+from transforms import rotate_image, detect_face
 
 token = os.environ['TOKEN']
 bot = telebot.TeleBot(f'{token}')
 
 def get_photo(message):   
-    fileID = message.photo[-1].file_id   
+    fileID = message.photo[-1].file_id
     file_info = bot.get_file(fileID)
     downloaded_file = bot.download_file(file_info.file_path)
-    with open("input_photo/image.jpg", 'wb') as new_file:
+    with open("telegram_bot/input_photo/image.jpg", 'wb') as new_file:
         new_file.write(downloaded_file)
     
     bot.send_message(message.from_user.id, 'Фото получено')
 
-def send_photo(message):   
-    photo = open('input_photo/image.jpg', 'rb')
+def send_photo(message):
+    detect_face('telegram_bot/input_photo/', 'telegram_bot/output_photo/')
+    photo = open('telegram_bot/output_photo/image.jpg', 'rb')
     bot.send_photo(message.from_user.id, photo)
 
     bot.send_message(message.from_user.id, 'Фото отправлено')
@@ -23,13 +25,13 @@ def get_text(message):
     name = message.text
     bot.send_message(message.from_user.id, 'Сообщение получено')
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=['text', 'photo'])
 def get_text_messages(message):
     if message.text == "/help":
         bot.send_message(message.from_user.id, "Вызови /send что бы отправить фото")
     elif message.text == "/send":
         bot.send_message(message.from_user.id, "Отправляй фото")
-        bot.register_next_step_handler(message, get_text)
+        # bot.register_next_step_handler(message, get_text)
         bot.register_next_step_handler(message, get_photo)
         bot.register_next_step_handler(message, send_photo)
     else:
--- telegram_bot/transforms.py
+++ telegram_bot/transforms.py
@@ -1,6 +1,17 @@
 import cv2
 from scipy import ndimage
 import matplotlib.pyplot as plt
+from super_gradients.training import models
+import torch
+import os
+
+DEVICE = 'cuda' if torch.cuda.is_available() else "cpu"
+model_path = '/home/bazilyq/Рабочий стол/MLSD/photo_anonymization/telegram_bot/models'
+best_model = models.get(
+    'yolo_nas_l',
+    num_classes=1,
+    checkpoint_path=f"{model_path}/average_model.pth"
+).to(DEVICE)
 
 
 def rotate_image(input_path='input_photo', output_path='output_photo', show=False):
@@ -15,3 +26,35 @@ def rotate_image(input_path='input_photo', output_path='output_photo', show=Fals
     if show:
         plt.imshow(rotated)
         plt.show()
+
+
+def get_blure_image(predict):
+    img = predict._images_prediction_lst[0].image.copy()
+
+    for bb in predict._images_prediction_lst[0].prediction.bboxes_xyxy:
+        bb = list(bb)
+
+        x = bb[0]
+        y = bb[1]
+        width = bb[2] - x
+        height = bb[3] - y
+
+        blur_x = int(x)
+        blur_y = int(y)
+        blur_width = int(width)
+        blur_height = int(height)
+
+        roi = img[blur_y:blur_y+blur_height, blur_x:blur_x+blur_width]
+        blur_image = cv2.GaussianBlur(roi,(51,51),0)
+
+        img[blur_y:blur_y+blur_height, blur_x:blur_x+blur_width] = blur_image
+
+    return img
+
+
+def detect_face(input_path='telegram_bot/input_photo', output_path='telegram_bot/output_photo', show=False):
+    in_photo_path = f'{input_path}/image.jpg'
+    predict = best_model.predict(in_photo_path)
+    blured_img = get_blure_image(predict)
+    cv2.imwrite(f"{output_path}/image.jpg", blured_img)
+
